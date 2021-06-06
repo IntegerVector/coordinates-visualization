e1c3cbb1fe4692b2a5b9ee2642bbbca32d1c13de
IntegerVector <andrii.ruban@outlook.com>
2021-06-06 22:01:39 +0300

    styles moved to html file

--- src/main.js
+++ src/main.js
@@ -10,19 +10,11 @@ board.filter(isPrime);
 board.draw(10000, 10000, dotsCount);
 
 function isPrime(number) {
-  if(number == 2)
-        return true;
+  if(number == 2) return true;
+  if(number < 2 || number % 2 == 0) return false;
 
-    if(number < 2 || number % 2 == 0)
-        return false;
+  for(let i = 3; i*i <= number; i += 2)
+    if(number % i == 0) return false;
 
-    /*
-     * we only need to check until the sqrt
-     * and we can omit the even numbers as well
-     */
-    for(let i = 3; i*i <= number; i += 2)
-        if(number % i == 0)
-            return false;
-
-    return true;
+  return true;
 }
